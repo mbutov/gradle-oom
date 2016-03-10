153f380bbe076908cbad518e1927bbd24cb7fa09
Maxim Butov <mbutov@qulix.com>
2016-03-10 13:54:45 +0300

    some comments

--- src/test/java/gradleoom/OOMTest.java
+++ src/test/java/gradleoom/OOMTest.java
@@ -12,8 +12,16 @@ public class OOMTest {
 
         System.out.println("Generating many lines of output");
 
-        // 1M iterations is enough for Gradle (-Xmx128m), 1G - for Idea (-Xmx4096m)
-        for (int totalEols = 0; totalEols < 1000 * 1000; totalEols++) {
+        // "gradle test" will throw "java.lang.OutOfMemoryError: GC overhead limit exceeded"
+        //
+        // Idea 15 (143.2287) with memory option -Xmx4096m runs this test for 1-2 minutes
+        // then it consumes all CPU time.
+        //
+        // Tested on Ubuntu 14.04, zulu8.13.0.5-jdk8.0.72-linux_x64.
+        //
+        // Before running this test, start "sleep 180 && pkill -9 -f idea" in terminal.
+
+        for (;;) {
 
             System.out.println();
 
